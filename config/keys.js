f5903f4d8449f1d77396e07644f071d2ceea8454
Susan Cagle <susyjustice@gmail.com>
2020-10-18 14:00:10 -0400

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-const MONGODB = {
-    MONGODB_URI: "mongodb+srv://admin:i8szbxclgPm5DpQY@cluster0.irwke.mongodb.net/Columbia?retryWrites=true&w=majority"
-};
-
-// module.exports = KEYS;
-module.exports = MONGODB;
-
