9bfedf1011f047a99ca034afc5a15ebcb4e3a8fc
BubblyRobot <susyjustice@gmail.com>
2020-10-18 13:54:16 -0400

    Got the database working

---
+++ server.js
@@ -0,0 +1,41 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys.js");
+
+
+
+// app config variables
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+const port = process.env.PORT || 9000;
+
+// /////////////////  MIDDLEWARE //////////////////////////////////
+app.use(express.json());
+
+
+////////////// Config //////////////////////////////////////////
+//DB config
+mongoose.connect(keys.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*"),
+        res.setHeader("Access-Control-Allow-Headers", "*"),
+        next();
+});
+
+// routes
+// routes
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+// listen
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
